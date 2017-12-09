7fca20e6a6b2619f24523244e47440831c13f99a
helguera <javier.helguera@alumnos.uva.es>
2017-12-09 21:13:02 +0100

    creado la comprobacion de atributos y el socket

--- Practica 4/cliente.c
+++ Practica 4/cliente.c
@@ -15,29 +15,42 @@
 
 #include "ip-icmp-ping.h"
 
+int sockfd;
 int desarrollado=0;
+
+struct in_addr addr;
 
 
 int main(int argc, char *argv[]){
 
-
-    if(argc==2 || argc==3){
-        if(argc==2){
-
-        }else{
-
-        }
-    }else{
+    if (argc !=2 || argc !=3){
         fprintf(stderr,"uso: %s ip-servidor [-v]\n", argv[0]);
         exit(EXIT_FAILURE);
+    }else{
+        if(inet_aton(argv[1], &addr)==0){
+            fprintf(stderr, "Direccion ip no valida\n");
+            exit(EXIT_FAILURE);
+        }
+        if(argc==3){
+            if(strcmp(argv[2],"-v")==0){
+                desarrollado=1;
+            }else{
+                fprintf(stderr,"uso: %s ip-servidor [-v]\n", argv[0]);
+                exit(EXIT_FAILURE);
+            }
+        }
     }
 
 //**********************************************************************************
 
-ECHORequest echoRequest;
-ECHOResponse echoResponse;
+    ECHORequest echoRequest;
+    ECHOResponse echoResponse;
 
-
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sockfd == -1){
+        fprintf(stderr, "Error al crear el socket");
+        exit(EXIT_FAILURE);
+    }
 
 
 

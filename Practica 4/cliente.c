40d30740a83f0f050aecf9517b4152bd2c4ab11f
helguera <javier.helguera@alumnos.uva.es>
2017-12-12 10:54:02 +0100

    Inicializacion de las variables y comienzo del checksum

--- Practica 4/cliente.c
+++ Practica 4/cliente.c
@@ -15,10 +15,14 @@
 
 #include "ip-icmp-ping.h"
 
-int sockfd;
+//Cmabiar el nombre al socket enlace
+int sockfd, enlace;
 int desarrollado=0;
+char ip[20];                       //guarda la ip
 
 struct in_addr addr;
+struct sockaddr_in local, remoto;
+socklen_t addr_size;
 
 
 int main(int argc, char *argv[]){
@@ -31,6 +35,8 @@ int main(int argc, char *argv[]){
             fprintf(stderr, "Direccion ip no valida\n");
             exit(EXIT_FAILURE);
         }
+        strcpy(ip, argv[1]);
+
         if(argc==3){
             if(strcmp(argv[2],"-v")==0){
                 desarrollado=1;
@@ -52,7 +58,60 @@ int main(int argc, char *argv[]){
         exit(EXIT_FAILURE);
     }
 
+    remoto.sin_family=AF_INET;
+    remoto.sin_port=0;
+    remoto.sin_addr.s_addr = inet_addr(ip);
+    //memset(remoto.sin_zero, '\0', sizeof remoto.sin_zero);
+    //addr_size = sizeof remoto;
+
+    local.sin_family=AF_INET;
+    local.sin_port=0;
+    local.sin_addr.s_addr =INADDR_ANY;
+
+	if (bind(sockfd, (struct sockaddr *) &local, sizeof(struct sockaddr_in)) == -1) {
+		perror("Error al hacer el bind\n");
+		exit(EXIT_FAILURE);
+	}
+
+    bzero(&echoRequest, sizeof(echoRequest));
+    echoRequest.ID=getpid();
+    echoRequest.SeqNumber=0;
+    strcpy(echoRequest.payload, "payload");
+
+    echoRequest.icmpHeader.Type=8;
+    echoRequest.icmpHeader.Code=0;
+
+    //Calculo del Checksum
+    int numShorts=sizeof(echoRequest)/2;
+    unsigned short int *puntero;
+    int acumulado=0;
+    puntero=(unsigned short int *) &echoRequest;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//**********************************************************************************
     return(EXIT_SUCCESS);
 }
